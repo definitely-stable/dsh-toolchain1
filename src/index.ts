89d4bdfe6a1948b0248877b2c24db5470f764e71
MrFr3di <ynagi-wm@mail.ru>
2026-08-26 13:21:30 +0500

    feat(package): expose minimal root API

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { createApplicationKernel } from './kernel/index.js'
+export type { ApplicationKernel, KernelDescriptor } from './kernel/index.js'
+export { TOOLCHAIN_PRODUCT, TOOLCHAIN_VERSION } from './product.js'
+export { TOOLCHAIN_PROTOCOL_VERSION } from './protocol/index.js'
