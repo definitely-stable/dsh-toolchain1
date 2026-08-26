455b54b508a8bfe16b44a05829d1eb2667db319f
MrFr3di <ynagi-wm@mail.ru>
2026-08-26 17:42:52 +0500

    refactor(api): keep application kernel internal

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,2 @@
-export { createApplicationKernel } from './kernel/index.js'
-export type { ApplicationKernel, KernelDescriptor } from './kernel/index.js'
 export { TOOLCHAIN_PRODUCT, TOOLCHAIN_VERSION } from './product.js'
 export { TOOLCHAIN_PROTOCOL_VERSION } from './protocol/index.js'
